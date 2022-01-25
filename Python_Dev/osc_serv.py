9a94c47a281a6162f30f85aeef5ba69fd89d0387
Lino Delpech <delpechlino@gmail.com>
2022-01-25 09:19:54 +0100

    Functionnal Osc Communication

--- Python_Dev/osc_serv.py
+++ Python_Dev/osc_serv.py
@@ -3,14 +3,31 @@ from os import O_SYNC
 import argparse
 import math
 
+from defs import OscCallbacks
+
 from pythonosc import dispatcher
 from pythonosc import osc_server
+from pythonosc import udp_client
+
+Call = OscCallbacks
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--ip",default="127.0.0.1", help="the ip to listen on")
+parser.add_argument("--ip",default="192.168.145.172", help="the ip to listen on")
 parser.add_argument("--port", type=int, default=5005,help="the ip to listen on")
 args = parser.parse_args()
 
 dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/EggControl",Call.egg_control,"activation")
+dispatcher.map("/Move/Base",Call.move_base,"targetAngle", "timeToReach", "accelTime")
+dispatcher.map("/Move/Shoulder",Call.move_shoulder,"targetAngle", "timeToReach", "accelTime")
+dispatcher.map("/Move/Elbow",Call.move_elbow,"targetAngle", "timeToReach", "accelTime")
+dispatcher.map("/Move/WristVer",Call.move_wrist_ver,"targetAngle", "timeToReach", "accelTime")
+dispatcher.map("/Move/WristRot",Call.move_wrist_rot,"targetAngle", "timeToReach", "accelTime")
+dispatcher.map("/Move/Gripper",Call.move_gripper,"targetAngle", "timeToReach", "accelTime")
 
-dispatcher.map("")
+server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+print("Serving on {}".format(server.server_address))
+
+server.serve_forever()
---
+++ Python_Dev/test_osc_client.py
@@ -0,0 +1,38 @@
+
+import argparse
+import random
+import time
+import math
+from pickle import FALSE, TRUE
+from pythonosc import udp_client
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="192.168.145.172",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    activation = TRUE
+    targetAngle = 0
+    timeToReach = 3600
+    for x in range(3600):
+        activation = not activation
+        accelTime = timeToReach/4
+        timeToReach = timeToReach-1
+        targetAngle =targetAngle+1
+        if targetAngle > 360:
+            targetAngle = 0
+        client.send_message("/EggControl", [activation])
+        client.send_message("/Move/Base",[targetAngle,timeToReach,accelTime])
+        client.send_message("/Move/Shoulder",[targetAngle,timeToReach,accelTime])
+        client.send_message("/Move/Elbow",[targetAngle,timeToReach,accelTime])
+        client.send_message("/Move/WristVer",[targetAngle,timeToReach,accelTime])
+        client.send_message("/Move/WristRot",[targetAngle,timeToReach,accelTime])
+        client.send_message("/Move/Gripper",[targetAngle,timeToReach,accelTime])
+        activation = not activation
+        client.send_message("/EggControl", [activation])
+        time.sleep(1)
