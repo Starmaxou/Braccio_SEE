fd102bb93df75d94bb606b8d27413f042b919030
Lino Delpech <delpechlino@gmail.com>
2022-02-17 08:31:56 +0100

    added main.py file

--- src/Python_Dev/OSC/defs.py
+++ src/Python_Dev/OSC/defs.py
@@ -2,49 +2,66 @@ from os import O_SYNC
 import argparse
 import math
 
-class OscCallbacks :
-    def egg_control(self, str, args, *stArgs):
-        _Activation = args
-        print("EggControl :", _Activation)
+from main import Base, Shoulder, Elbow, WristVer, WristRot, Gripper
 
+class OscCallbacks:
+
+    _eggActivated = False
+
+    def egg_control(self, str, args, *stArgs):
+        self._eggActivated = args
+        print("EggControl :", self._eggActivated)
+        # TODO Insérer ici la fonction à appeler pour exécuter la commande
+    
     def move_base(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveBase -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        Base.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
 
+        if not(self._eggActivated) :
+            Base.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveBase -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
+    
     def move_shoulder(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveShoulder -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        Shoulder.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+        if not(self._eggActivated) :
+            Shoulder.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveShoulder -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
+
 
     def move_elbow(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveElbow -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        Elbow.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+        if not(self._eggActivated) :
+            Elbow.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveElbow -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
         
+
     def move_wrist_ver(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveWristVer -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        WristVer.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+        if not(self._eggActivated) :
+            WristVer.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveWristVer -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
+    
 
     def move_wrist_rot(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveWristRot -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        WristRot.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+        if not(self._eggActivated) :
+            WristRot.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveWristRot -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
+
 
     def move_gripper(self, str, args, *stArgs):
         _targetAngle = args
         _timeToReach = stArgs[0]
         _accelTime = stArgs[1]
-        print("MoveGripper -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
-        Gripper.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+        if not(self._eggActivated) :
+            Gripper.move(_targetAngle, _timeToReach, _accelTime, True, False, False)
+            print("MoveGripper -> targetAngle :", _targetAngle, " timeToReach :", _timeToReach, "accelTime", _accelTime)
